d998edcc37a92e29eee330db9639fa9ec9e6b614
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:53:33 +0900

    Update Chapter08.c

--- Chapter08.c
+++ Chapter08.c
@@ -1,69 +1,22 @@
 #include <stdio.h>
-#include <string.h>
- 
-struct phone {
-    char name[20];
-    char number[20];
-    int age;
-};
- 
-void phone_sort(struct phone[], int);
-void phone_print(struct phone[], int);
-void call(int n);
 
-struct phone table[10];
-int count;
- 
-int main()
-{
-    printf("등록할 정보 입력(최대 10명)\n");
-    printf("그만입력하려면 0 입력\n");
+int main(void) {
+    int distance, speed;
+    int hour, min;
+    double time;
+    double seconds;
 
-    call(10);
+    printf("거리, 속력 입력 >> "); // 시간 = 거리 / 속력
+    scanf("%d %d", &distance, &speed);
 
-    phone_sort(table, count);
-    phone_print(table, count);
+    time = (double)distance / speed * 3600;
+    hour = time / 3600;
+    time = time - (hour * 3600);
+    min = time / 60;
+    time = time - (min * 60);
+    seconds = time;
+
+    printf("소요 시간은 %d시간 %d분 %3.lf초입니다.", hour, min, seconds);
+
     return 0;
-}
- 
-void phone_sort(struct phone table[], int count)
-{
-    int i, j;
-    struct phone temp;
-    for (i = 0; i < count - 1; i++)
-        for (j = i + 1; j < count; j++)
-            if (strcmp(table[i].name, table[j].name) == 1)
-            {
-                temp = table[i];
-                table[i] = table[j];
-                table[j] = temp;
-            }
-}
- 
-void phone_print(struct phone table[], int count)
-{
-    int i;
-    printf("\n---------------------------------\n");
-    printf("%8s %16s %6s\n", "이름", "전화번호", "나이");
-    printf("---------------------------------\n");
-    for (i = 0; i < count; i++)
-        printf("%8s %16s %6d\n", table[i].name, table[i].number, table[i].age);
-    printf("---------------------------------\n\n");
-}
-
-void call(int n) {
-    if(n == 0) return;
-
-    count++;
-    printf("이름 입력 : ");
-    scanf_s("%s", table[n].name, sizeof(table[n].name));
-    if (strcmp(table[n].name, "0") == 0)
-        return;
-    printf("전화번호 입력 : ");
-    scanf_s("%s", table[n].number, sizeof(table[n].number));
-    printf("나이 입력 : ");
-    scanf_s("%d", &table[n].age);
-    fflush(stdin);
-
-    call(n-1);
 }
