4600382a542768d7727715cca838e17979efbcb0
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:45:04 +0900

    Update Chapter06.c

--- Chapter06.c
+++ Chapter06.c
@@ -1,23 +1,64 @@
-#include<stdio.h>
-#include<memory.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+ 
+struct phone {
+    char name[20];
+    char number[20];
+    int age;
+};
+ 
+void phone_sort(int);
+void phone_print(int);
+void call(int n);
+ 
+struct phone table[10];
+int main()
+{
+    int count;
+    printf("등록할 정보 입력(최대 10명)\n");
+    printf("그만입력하려면 0 입력\n");
+    call(10);
+    phone_sort(count);
+    phone_print(count);
+    return 0;
+}
+ 
+void phone_sort(int count)
+{
+    int i, j;
+    struct phone temp;
+    for (i = 0; i < count - 1; i++)
+        for (j = i + 1; j < count; j++)
+            if (strcmp(table[i].name, table[j].name) == 1)
+            {
+                temp = table[i];
+                table[i] = table[j];
+                table[j] = temp;
+            }
+}
+ 
+void phone_print(int count)
+{
+    int i;
+    printf("\n---------------------------------\n");
+    printf("%8s %16s %6s\n", "이름", "전화번호", "나이");
+    printf("---------------------------------\n");
+    for (i = 0; i < count; i++)
+        printf("%8s %16s %6d\n", table[i].name, table[i].number, table[i].age);
+    printf("---------------------------------\n\n");
+}
 
-#define MAX_NLEN 20 //Max Name Length
-#define MAX_SUB 3   //Max Subject Number
-#define MAX_STU 20  //Max Student Number
-typedef struct _s{
-	char name[MAX_NLEN + 1];
-	int stuNum;//학번
-	int scores[MAX_SUB];//국영수 과목 점수
-}Student;
+void call(int n) {
+    if(n == 0) return;
+    printf("이름 입력 : ");
+    scanf("%s", table[n].name, sizeof(table[n].name));
+    if (strcmp(table[n].name, "0") == 0)
+        return;
+    printf("전화번호 입력 : ");
+    scanf("%s", table[n].number, sizeof(table[n].number));
+    printf("나이 입력 : ");
+    scanf("%d", &table[n].age);
+    fflush(stdin);
 
-const char* subjName[MAX_SUB] = { "국어","영어","수학" };
-Student gstu[MAX_STU];//global variable student
-void Init();//초기화
-void Run();
-int main() {
-	Init();
-	Run();
-	return 0;
+    call(n-1);
 }
