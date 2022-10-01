24d0a5b2dfc197c9ab83773bc0cbf458c8e59b44
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:49:48 +0900

    Update Chapter07.c

--- Chapter07.c
+++ Chapter07.c
@@ -1,92 +1,17 @@
 #include <stdio.h>
+int main(void) {
+    int weigh;
+    double height;
+    double BMI;
+    printf("몸무게(kg)와 키(cm) 입력 >> ");
+    scanf("%d%lf", &weigh, &height);
 
-int Add(int A, int B)
-{
-	return A + B;
-}
+    BMI = weigh / ((height * 0.01) * (height * 0.01));
 
-int Sub(int A, int B)
-{
-	return A - B;
-}
+    if (BMI < 25 && BMI >= 20)
+        printf("표준입니다.\n");
+    else
+        printf("체중 관리가 필요합니다.\n");
 
-int Mul(int A, int B)
-{
-	return A * B;
-}
-
-int Div(int A, int B)
-{
-	return A / B;
-}
-
-void HowTo1(void)
-{
-	printf("사칙연산 프로그램입니다.\n");
-	printf("두 정수를 입력해주세요: ");
-}
-
-void HowTo2(void)
-{
-	printf("어떤 계산을 할까요?\n");
-	printf("1. 덧셈 2. 뺄셈 3. 곱셈 4. 나눗셈");
-}
-
-int ReadNum(void)
-{
-	int num;
-	scanf_s("%d", &num);
-	return num;
-}
-
-int main(void)
-{
-	int opt, result, num1, num2, A, B;
-	
-	HowTo1();
-
-	num1 = ReadNum();
-	num2 = ReadNum();
-
-	if (num1 > num2)
-	{
-		A = num1;
-		B = num2;
-	}
-	else
-	{
-		A = num2;
-		B = num1;
-	}
-
-	while (1)
-	{
-
-		HowTo2();
-		opt = ReadNum();
-
-		if (opt < 1 || opt>4)
-		{
-			printf("잘못된 숫자\n");
-			continue;
-		}
-		break;
-	}
-
-	switch (opt)
-	{
-	case 1:
-		printf("%d+%d=%d\n", A, B, Add(A, B));
-		break;
-	case 2:
-		printf("%d-%d=%d\n", A, B, Sub(A, B));
-		break;
-	case 3:
-		printf("%d*%d=%d\n", A, B, Mul(A, B));
-		break;
-	case 4:
-		printf("%d/%d=%d\n", A, B, Div(A, B));
-		break;
-	}
-	
+    return 0;
 }
